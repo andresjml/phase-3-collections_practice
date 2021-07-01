e29129f4f4f3f4cac8c317aad6acd8695e7bc011
Andres <andresjml@gmail.com>
2021-07-01 19:02:27 -0400

    completed assignment

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,51 @@
+def sort_array_asc(integers)
+  integers.sort
+end
 
+def sort_array_desc(integers)
+  integers.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(strings)
+  strings.sort do |a,b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements (array)
+  array[1],array[2]=array[2],array[1]
+  array
+end
+
+def reverse_array (integers)
+  integers.reverse
+end
+
+def kesha_maker(strings)
+  strings.each do |string|
+    string[2]="$"
+  end
+end
+
+def find_a(strings)
+  strings.select do |string|
+    string[0]=="a"
+  end
+end
+
+def sum_array(array)
+  array.reduce(0) {|sum, num| sum+num}
+end
+
+def add_s(array)
+  array.collect do |element|
+    if array[1]==element
+      element
+    else
+      element+"s"
+    end
+
+  end
+end
